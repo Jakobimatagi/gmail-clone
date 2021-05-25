6c3d038cf0d6fadeffa501549440f6a34d2f941d
Jakobi Matagi <jkmatagi@hotmail.com>
2021-05-25 12:25:14 -0600

    adding the email list sections

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 // components
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Mail from './components/Mail';
+import EmailList from './components/EmailList';
+
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+            <Route path="/">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+        
+    
+      </div>
+    </Router>
   );
 }
 
